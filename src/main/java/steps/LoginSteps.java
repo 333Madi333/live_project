757f14ba4f39723898182323d435d37308a8e584
Tunjira Hunhaboon <aommie85@gmail.com>
2021-11-13 14:10:48 -0500

    update title from Home Page to Strudent Portal

--- src/main/java/steps/LoginSteps.java
+++ src/main/java/steps/LoginSteps.java
@@ -26,7 +26,7 @@ public class LoginSteps {
 
     @Then("Title of the page should be Home Page")
     public void titleOfThePageShouldBeHomePage() {
-        Assert.assertEquals("Home Page" , WebDriverUtils.getDriver().getTitle());
+        Assert.assertEquals("Student Portal" , WebDriverUtils.getDriver().getTitle());
         WebDriverUtils.quitDriver();
     }
 
